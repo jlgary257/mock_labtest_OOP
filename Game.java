ebe3d699f1d0b0159d66dae907249bba6a313071
JOEL GARY <b032110294@student.utem.edu.my>
2023-05-15 21:31:22 +0800

    meow

---
+++ Game.java
@@ -0,0 +1,25 @@
+public class Game
+{
+	private String name;
+	private double price;
+	
+	public Game()
+	{}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public double getPrice()
+	{
+		return price;
+	}
+	public setPrice(double price)
+	{
+		this.price = price;
+	}
+	
+}
---
+++ Subscriber.java
@@ -0,0 +1,16 @@
+public class Subscriber
+{
+	private boolean isSubscriber;
+	private char type;
+
+	public Subscriber()
+	{}
+	public boolean isSubscriber()
+	{
+
+	}
+	public void setSubscriber(boolean isSubscriber)
+	{
+		
+	}
+}
